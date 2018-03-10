1e14afb1316529e1f46cfce8d1e45776530bde1d
BBedaMoc <dygus93@wp.pl>
2018-03-10 13:12:05 +0100

    Version 2.01

--- main.cpp
+++ main.cpp
@@ -10,8 +10,7 @@ int main(int argc, char *argv[])
 {
 
     QApplication a(argc, argv);
-    MainWindow* w;
-    int k=9;
+    MainWindow* w; 
     w = new MainWindow();
     w->setAttribute(Qt::WA_DeleteOnClose);    
     w->show();
